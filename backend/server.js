43c0f231aa296fea4307c56bd4fd28d38288bb79
Urvashi <urvashirichaagrawal@gmail.com>
2024-06-03 20:04:07 +0530

    h

--- backend/server.js
+++ backend/server.js
@@ -17,19 +17,7 @@ const PORT = process.env.PORT || 3001
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 
 // Set up CORS middleware
-const allowedOrigins = ["https://query-doc-ra4f.vercel.app"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["POST", "GET"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json()); // Add this line to parse JSON requests
 
 // Set up storage for uploaded files
