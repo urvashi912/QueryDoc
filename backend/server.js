b79265afa6ef2127ae4e92263d1789e01a2a6bb8
Urvashi <urvashirichaagrawal@gmail.com>
2024-06-04 23:17:14 +0530

    x

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json()); // Add this line to parse JSON requests
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/tmp/');
+    cb(null, path.join(__dirname,'/tmp/'));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
