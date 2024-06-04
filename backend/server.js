397a211cb176462b7e2307d16c4e78ab528e4e87
Urvashi <urvashirichaagrawal@gmail.com>
2024-06-04 21:42:48 +0530

    f

--- backend/server.js
+++ backend/server.js
@@ -86,6 +86,10 @@ app.post("/ask", async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.json("heloooooooooooooooo")
+})
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
